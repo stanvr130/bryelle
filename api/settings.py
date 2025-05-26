da5e9acaa940e48352ca445cf0a39e74d04157da
stanvr130 <stanleyokafor08@gmail.com>
2025-05-26 07:23:42 -0700

    message

--- api/settings.py
+++ api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bryelle.onrender.com' ]
 SECRET_KEY = config('Secret_key')
 
 
